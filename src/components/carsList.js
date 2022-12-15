6237d9b632af5cb5f5608a1230251bff02fbad73
SlaviGrigorov <slavibg@mail.bg>
2022-12-15 21:22:16 +0200

    AddedKeys
    - Updated CarList component
    - added keys to the children (li elements)

--- src/components/carsList.js
+++ src/components/carsList.js
@@ -1,11 +1,12 @@
 import { Car } from './CarComponent';
 
 export const CarList = (props) => {
-    let carsList = props.cars.map(car => <Car {...car}/>)
+    // let carsList = props.cars.map(car => <Car {...car}/>)
 
     return (
         <ul>
-            {carsList}
+            {/* {carsList} */}
+            {props.cars.map((x, i) => <Car key={i + {...x}} {...x} />)}
         </ul>
     );
 }
